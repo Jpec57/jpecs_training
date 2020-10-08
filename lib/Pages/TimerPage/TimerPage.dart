77072bcf5a163c3f7e2525e87440c49f20f2d715
jpbella <bella@leboncandidat.fr>
2020-10-08 14:01:30 +0200

    Timer

--- lib/Pages/TimerPage/TimerPage.dart
+++ lib/Pages/TimerPage/TimerPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jpec_training/AppColors.dart';
 import 'package:jpec_training/Widgets/DefaultScaffold.dart';
 
 class TimerPage extends StatefulWidget {
@@ -7,19 +8,102 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
+  int _currentSet = 6;
+  int _countdown = 0;
+
   Widget _renderTimerButton(int time) {
     return Expanded(
+      child: InkWell(
+        onTap: (){
+          setState(() {
+            _countdown = time;
+          });
+        },
         child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: RaisedButton(
-        onPressed: () {},
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Text(
-          "$time",
-          style: Theme.of(context).textTheme.headline1,
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            onPressed: () {},
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Text(
+              "$time",
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
         ),
       ),
-    ));
+    );
+  }
+
+  Widget _renderTimerRow(int time1, int time2) {
+    return Expanded(
+      flex: 2,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _renderTimerButton(time1),
+          _renderTimerButton(time2),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderSet(int setNumber) {
+    Color backgroundColor = AppColors.richBlack;
+    Color textColor = Colors.white;
+    if (_currentSet == setNumber) {
+      backgroundColor = AppColors.greenArtichoke;
+      textColor = Colors.white;
+    }
+    return Expanded(
+        child: InkWell(
+          onTap: (){
+            setState(() {
+              _currentSet = setNumber;
+            });
+          },
+          child: Container(
+      decoration: BoxDecoration(
+          color: AppColors.richBlack,
+      ),
+      child: Center(
+            child: Container(
+                decoration: BoxDecoration(
+                  color: backgroundColor,
+                    border:
+                        Border.all(color: AppColors.charlestonGreen, width: 3),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    '$setNumber',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: textColor),
+                  ),
+                ))),
+    ),
+        ));
+  }
+
+  Widget _renderSetRow() {
+    return Expanded(
+      flex: 1,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _renderSet(1),
+          _renderSet(2),
+          _renderSet(3),
+          _renderSet(4),
+          _renderSet(5),
+          _renderSet(6),
+        ],
+      ),
+    );
   }
 
   @override
@@ -29,26 +113,10 @@ class _TimerPageState extends State<TimerPage> {
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              _renderTimerButton(25),
-              _renderTimerButton(60),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              _renderTimerButton(25),
-              _renderTimerButton(60),
-            ],
-          ),
-        ),
+        _renderSetRow(),
+        _renderTimerRow(25, 60),
+        _renderTimerRow(90, 120),
+        _renderTimerRow(240, 360),
       ],
     ));
   }
