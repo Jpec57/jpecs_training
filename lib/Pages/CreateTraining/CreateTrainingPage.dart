7cb59e0616dbbcfbcee0b44e06189ad847df2dcc
jpbella <bella@leboncandidat.fr>
2020-12-22 13:48:52 +0100

    Ok opk

--- lib/Pages/CreateTraining/CreateTrainingPage.dart
+++ lib/Pages/CreateTraining/CreateTrainingPage.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:jpec_training/AppColors.dart';
 import 'package:jpec_training/Enums/MuscleEnum.dart';
 import 'package:jpec_training/Models/Exercise.dart';
+import 'package:jpec_training/Models/ExerciseSet.dart';
 import 'package:jpec_training/Pages/HomePage/HomePage.dart';
 import 'package:jpec_training/Widgets/DefaultButton.dart';
 import 'package:jpec_training/Widgets/DefaultScaffold.dart';
@@ -99,75 +102,83 @@ class _CreateTrainingPageState extends State<CreateTrainingPage>
             child: ListView.separated(
                 shrinkWrap: true,
                 itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: AppColors.greenArtichokeDarker),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                Get.dialog(ExerciseExplanationDialog());
-                              },
-                              child: Image.asset(
-                                'images/jpec_logo.png',
-                                height: 90,
-                              ),
-                            ),
-                            Expanded(
-                              flex: 4,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Exercise name",
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    Text(
-                                      "Triceps",
-                                      style: TextStyle(
-                                          color: Colors.black45,
-                                          fontSize: 16,
-                                          fontStyle: FontStyle.italic),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 8.0),
-                                      child: Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          Icon(
-                                            Icons.fitness_center,
-                                            size: 16,
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                left: 5, right: 5),
-                                            child: Text(
-                                              "Pull up bar",
-                                              style: TextStyle(
-                                                color: Colors.black45,
-                                                fontSize: 16,
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
+                  return GestureDetector(
+                    onTap: (){
+                      print(_exercises);
+                      _exercises.add(new Exercise(name: 'Toto', sets: [new ExerciseSet(repsOrDuration: 30, rest: 50)]));
+                      _tabController.index = TAB_WORKOUT_INDEX;
+                      print(_exercises);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: AppColors.greenArtichokeDarker),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  Get.dialog(ExerciseExplanationDialog());
+                                },
+                                child: Image.asset(
+                                  'images/jpec_logo.png',
+                                  height: 90,
                                 ),
                               ),
-                            ),
-                          ],
+                              Expanded(
+                                flex: 4,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Exercise name",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Text(
+                                        "Triceps",
+                                        style: TextStyle(
+                                            color: Colors.black45,
+                                            fontSize: 16,
+                                            fontStyle: FontStyle.italic),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 8.0),
+                                        child: Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.fitness_center,
+                                              size: 16,
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 5, right: 5),
+                                              child: Text(
+                                                "Pull up bar",
+                                                style: TextStyle(
+                                                  color: Colors.black45,
+                                                  fontSize: 16,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -214,54 +225,78 @@ class _CreateTrainingPageState extends State<CreateTrainingPage>
   }
 
   Widget _renderTrainingView() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          color: AppColors.beige,
-          onPressed: () {
-            Get.toNamed(HomePage.routeName);
-          },
-        ),
-        ExpansionPanelList(
-          expansionCallback: (int index, bool isExpanded) {
-            setState(() {
-              // items[index].isExpanded = !items[index].isExpanded;
-            });
-          },
-          children: _exercises.map((exercise) {
-            return ExpansionPanel(
-              headerBuilder: (BuildContext context, bool isExpanded) {
-                return ListTile(
-                    title: Text(
-                  exercise.name,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ));
-              },
-              isExpanded: true,
-              body: Text("Contenu"),
-            );
-          }).toList(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(12),
-          child: IconButton(
-              icon: Icon(
-                Icons.add_circle,
-                color: AppColors.beige,
-                size: 40,
-              ),
-              onPressed: () {
-                _tabController.index = TAB_EXERCISE_INDEX;
-              }),
-        ),
-        Container(),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            color: AppColors.beige,
+            onPressed: () {
+              Get.toNamed(HomePage.routeName);
+            },
+          ),
+          ExpansionPanelList(
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                // items[index].isExpanded = !items[index].isExpanded;
+              });
+            },
+            children: _exercises.map((exercise) {
+              return ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                      title: Text(
+                    exercise.name,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ));
+                },
+                isExpanded: true,
+                body: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Test"),
+                    ...exercise.sets.map((exercise){
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                            Text('Rest ${exercise.rest}'),
+                              Text('Reps ${exercise.repsOrDuration}'),
+                          ],)
+                        ],
+                      );
+                    }).toList(),
+                    IconButton(icon: Icon(Icons.add_circle), onPressed: (){
+                      exercise.sets.add(new ExerciseSet(repsOrDuration: 10, rest: 60));
+                      setState(() {
+
+                      });
+                    })
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: IconButton(
+                icon: Icon(
+                  Icons.add_circle,
+                  color: AppColors.beige,
+                  size: 40,
+                ),
+                onPressed: () {
+                  _tabController.index = TAB_EXERCISE_INDEX;
+                }),
+          ),
+          Container(),
+        ],
+      ),
     );
   }
 
